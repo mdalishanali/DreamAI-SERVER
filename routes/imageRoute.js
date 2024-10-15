4d59a67a5af608d140dd0420fc354b6d8dd83085
mdalishanali <md.alishanali88@gmail.com>
2024-10-15 23:45:32 +0530

    done

--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { generateImage } = require('../controllers/imageController');
+const { generateImage, getImages } = require('../controllers/imageController');
 
 const router = express.Router();
 
 router.post('/generate-image', generateImage);
-router.get('/discover-image', generateImage);
+router.get('/discover-image', getImages);
 
 module.exports = router;
